3e6f59b8a2e9aa33ca91efe4d85c6c99f6d426ff
Paran Sonthalia <paran.sonthalia@gmail.com>
2019-03-30 05:39:18 -0700

    recycle, compost, and landfill working with new model!!!!!

--- hardware/raspberryPi.py
+++ hardware/raspberryPi.py
@@ -6,28 +6,77 @@ import base64
 import json
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
+# from google.cloud import storage
+# from firebase import firebase
 
 
-servoPIN = 6
+servoPINTop = 6
+servoPINBottom = 13
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.setup(servoPINTop, GPIO.OUT)
+GPIO.setup(servoPINBottom, GPIO.OUT)
 camera = PiCamera()
+# client = storage.Client()
+# bucket = client.get_bucket('https://smartsort.firebaseio.com/')
+# firebase = firebase.FirebaseApplication('https://https://smartsort.firebaseio.com//pi/' + str(index), None)
 
-p = GPIO.PWM(servoPIN, 50)
-p.start(2.5)
+
+t = GPIO.PWM(servoPINTop, 50)
+t.start(7.2)
+b = GPIO.PWM(servoPINBottom, 50)
+b.start(7.2)
 imageIndex = 0
 
 try:
 	# 7.2 is stop
-	while True:
-		p.ChangeDutyCycle(6.5)
+	def recycle():
+		t.ChangeDutyCycle(7.8)
+		b.ChangeDutyCycle(7.9)
+		time.sleep(0.3)
+		t.ChangeDutyCycle(7.2)
+		b.ChangeDutyCycle(7.2)
+		time.sleep(2)
+		t.ChangeDutyCycle(6.33)
+		b.ChangeDutyCycle(6.2)
+		time.sleep(0.4)
+		t.ChangeDutyCycle(7.2)
+		b.ChangeDutyCycle(7.2)
+
+	def compost():
+		t.ChangeDutyCycle(7.8)
+		b.ChangeDutyCycle(5.75)
+		time.sleep(0.3)
+		t.ChangeDutyCycle(7.2)
+		b.ChangeDutyCycle(7.2)
+		time.sleep(2)
+		t.ChangeDutyCycle(6.33)
+		b.ChangeDutyCycle(7.73)
+		time.sleep(0.4)
+		t.ChangeDutyCycle(7.2)
+		b.ChangeDutyCycle(7.2)
+
+	def landfill():
+		t.ChangeDutyCycle(6.2)
 		time.sleep(0.2)
-		p.ChangeDutyCycle(7.2)
-		time.sleep(0.5)
-		p.ChangeDutyCycle(7.9)
-		time.sleep(0.18)
-		p.ChangeDutyCycle(7.2)
-		time.sleep(0.5)
+		t.ChangeDutyCycle(7.2)
+		time.sleep(2)
+		t.ChangeDutyCycle(7.7)
+		time.sleep(0.25)
+		t.ChangeDutyCycle(7.2)
+
+	while True:
+		landfill()
+		time.sleep(1)
+		# landfill()
+		# compost()
+		# t.ChangeDutyCycle(6.2)
+		# time.sleep(0.4)
+		# t.ChangeDutyCycle(7.2)
+		# time.sleep(2)
+		# t.ChangeDutyCycle(7.9)
+		# time.sleep(0.2)
+		# t.ChangeDutyCycle(7.2)
+		# time.sleep(2)
 
 		# camera.capture('/home/pi/Desktop/trashImages/image' + str(imageIndex) + '.jpg')
 
@@ -52,7 +101,19 @@ try:
 		# 	response = service_request.execute()
 		# 	print json.dumps(response, indent=4, sort_keys=True)
 
+		# blob = bucket.blob('root')
+		# blob.upload_from_filename(filename='/home/pi/Desktop/trashImages/image' + str(imageIndex) + '.jpg')
+		# firebase.patch('https://https://smartsort.firebaseio.com/pi/' + str(index), {photoIndex: True})
+
+
+
 
 except KeyboardInterrupt:
-	p.stop()
+	t.stop()
+	b.stop()
 	GPIO.cleanup()
+
+
+
+	
+	
