53d800cee9283b200916e62db1fd7955c5582bf4
Paran Sonthalia <paran.sonthalia@gmail.com>
2019-03-30 14:00:40 -0700

    lots and lots and lots and lots of updates to the RPi code

--- hardware/raspberryPi.py
+++ hardware/raspberryPi.py
@@ -1,14 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 from picamera import PiCamera
-import argparse
-import base64
-import json
-from googleapiclient import discovery
-from oauth2client.client import GoogleCredentials
-# from google.cloud import storage
-# from firebase import firebase
-
+from firebase import firebase
+from google.cloud import vision
+import io
+import os
 
 servoPINTop = 6
 servoPINBottom = 13
@@ -16,95 +12,109 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPINTop, GPIO.OUT)
 GPIO.setup(servoPINBottom, GPIO.OUT)
 camera = PiCamera()
-# client = storage.Client()
-# bucket = client.get_bucket('https://smartsort.firebaseio.com/')
-# firebase = firebase.FirebaseApplication('https://https://smartsort.firebaseio.com//pi/' + str(index), None)
+firebase = firebase.FirebaseApplication('https://smartsort.firebaseio.com', None)
 
 
 t = GPIO.PWM(servoPINTop, 50)
-t.start(7.2)
+t.start(7.5)
 b = GPIO.PWM(servoPINBottom, 50)
-b.start(7.2)
+b.start(7.5)
 imageIndex = 0
+
+client = vision.ImageAnnotatorClient()
+
+def recycle():
+	t.ChangeDutyCycle(7.8)
+	b.ChangeDutyCycle(8.0)
+	time.sleep(0.3)
+	t.ChangeDutyCycle(7.5)
+	b.ChangeDutyCycle(7.5)
+	time.sleep(2)
+	t.ChangeDutyCycle(7.3)
+	b.ChangeDutyCycle(6.95)
+	time.sleep(0.4)
+	t.ChangeDutyCycle(7.5)
+	b.ChangeDutyCycle(7.5)
+
+def compost():
+	t.ChangeDutyCycle(7.9)
+	b.ChangeDutyCycle(6.75)
+	time.sleep(0.3)
+	t.ChangeDutyCycle(7.5)
+	b.ChangeDutyCycle(7.5)
+	time.sleep(2)
+	t.ChangeDutyCycle(7.3)
+	b.ChangeDutyCycle(7.8)
+	time.sleep(0.4)
+	t.ChangeDutyCycle(7.5)
+	b.ChangeDutyCycle(7.5)
+
+def landfill():
+	t.ChangeDutyCycle(7.1)
+	time.sleep(0.2)
+	t.ChangeDutyCycle(7.5)
+	time.sleep(2)
+	t.ChangeDutyCycle(7.9)
+	time.sleep(0.25)
+	t.ChangeDutyCycle(7.5)
 
 try:
 	# 7.2 is stop
-	def recycle():
-		t.ChangeDutyCycle(7.8)
-		b.ChangeDutyCycle(7.9)
-		time.sleep(0.3)
-		t.ChangeDutyCycle(7.2)
-		b.ChangeDutyCycle(7.2)
-		time.sleep(2)
-		t.ChangeDutyCycle(6.33)
-		b.ChangeDutyCycle(6.2)
-		time.sleep(0.4)
-		t.ChangeDutyCycle(7.2)
-		b.ChangeDutyCycle(7.2)
-
-	def compost():
-		t.ChangeDutyCycle(7.8)
-		b.ChangeDutyCycle(5.75)
-		time.sleep(0.3)
-		t.ChangeDutyCycle(7.2)
-		b.ChangeDutyCycle(7.2)
-		time.sleep(2)
-		t.ChangeDutyCycle(6.33)
-		b.ChangeDutyCycle(7.73)
-		time.sleep(0.4)
-		t.ChangeDutyCycle(7.2)
-		b.ChangeDutyCycle(7.2)
-
-	def landfill():
-		t.ChangeDutyCycle(6.2)
-		time.sleep(0.2)
-		t.ChangeDutyCycle(7.2)
-		time.sleep(2)
-		t.ChangeDutyCycle(7.7)
-		time.sleep(0.25)
-		t.ChangeDutyCycle(7.2)
-
 	while True:
-		landfill()
-		time.sleep(1)
-		# landfill()
-		# compost()
-		# t.ChangeDutyCycle(6.2)
-		# time.sleep(0.4)
-		# t.ChangeDutyCycle(7.2)
-		# time.sleep(2)
-		# t.ChangeDutyCycle(7.9)
-		# time.sleep(0.2)
-		# t.ChangeDutyCycle(7.2)
-		# time.sleep(2)
+		camera.capture('/home/pi/Desktop/trashImages/image0.jpg')
 
-		# camera.capture('/home/pi/Desktop/trashImages/image' + str(imageIndex) + '.jpg')
+		with io.open('/home/pi/Desktop/trashImages/image0.jpg', 'rb') as image_file:
+			content = image_file.read()
+
+		image = vision.types.Image(content=content)
+
+		response = client.label_detection(image=image)
+		labels = response.label_annotations
+		itemAdded = ""
+
+		#Recycle
+		recycling = ["plastic", "paper", "metal", "aluminum", "can", "bottle", "jar", "glass", "jug", "electronic", "device", "tech", "cardboard", "floor"]
+		composting = ["veg", "fruit", "food", "grain", "bread", "coffee", "tea"]
+
+		dumped = False
+		for label in labels:
+			print(label.description)
+			if not dumped:
+				for i in recycling:
+					if i in label.description.lower():
+						recycle()
+						dumped = True
+						itemAdded = label.description
+						firebase.push('/TestData/Compost/', {"test":"test"})
+						break
+			else:
+				break
+		
+		if not dumped:
+			for label in labels:
+				if not dumped:
+					for i in composting:
+						if i in label.description.lower():
+							compost()
+							dumped = True
+							itemAdded = label.description
+							break
+				else:
+					break
+
+		if not dumped:
+			landfill()
+			dumped = True
+			if len(labels > 0):
+				itemAdded = labels[0].description
 
 
+		firebase.post('/TestData/', {"test":"test"})
+		# firebase.patch('https://https://smartsort.firebaseio.com/pi/' + str(imageIndex), {imageIndex: True})
+		imageIndex += 1
+		time.sleep(2)
 
-		# credentials = GoogleCredentials.get_application_default()
-		# service = discovery.build('vision', 'v1', credentials=credentials)
-
-		# with open('/home/pi/Desktop/trashImages/image' + str(imageIndex) + '.jpg', 'rb') as image:
-		# 	image_content = base64.b64encode(image.read())
-		# 	service_request = service.images().annotate(body={
-		# 		'requests': [{
-		# 			'image': {
-		# 				'content': image_content.decode('UTF-8')
-		# 			},
-		# 			'features': [{
-		# 				'type': 'LABEL_DETECTION',
-		# 				'maxResults': 10
-		# 			}]
-		# 		}]
-		# 	})
-		# 	response = service_request.execute()
-		# 	print json.dumps(response, indent=4, sort_keys=True)
-
-		# blob = bucket.blob('root')
-		# blob.upload_from_filename(filename='/home/pi/Desktop/trashImages/image' + str(imageIndex) + '.jpg')
-		# firebase.patch('https://https://smartsort.firebaseio.com/pi/' + str(index), {photoIndex: True})
-
+		break
 
 
 
